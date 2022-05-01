11bebe0be68c6dccf382a7ddc92db562f1941b1a
Hirono.Masatake@bcg.com <Hirono.Masatake@bcg.com>
2022-05-01 17:56:47 +0900

    further refactor date relevant methods out of Greeter class

--- ch03/greeter.py
+++ ch03/greeter.py
@@ -5,29 +5,33 @@ import locale
 locale.setlocale(locale.LC_TIME, "ja_JP.UTF-8")
 
 
+def _day():
+    day = datetime.datetime.now().strftime("%A")
+    return day
+
+
+def _part_of_day():
+    hour = datetime.datetime.now().hour
+    if hour <= 12:
+        part = "午前"
+    elif hour > 12 and hour <= 17:
+        part = "午後"
+    else:
+        part = "夜"
+    return part
+
+
+# Greeter class knows too much
+# Refactor the methods relevant to date out of this class
 class Greeter:
     def __init__(self, name):
         self.name = name
-        self.day = None
-        self.part = None
-
-    def _day(self):
-        self.day = datetime.datetime.now().strftime("%A")
-
-    def _part_of_day(self):
-        hour = datetime.datetime.now().hour
-        if hour <= 12:
-            self.part = "午前"
-        elif hour > 12 and hour <= 17:
-            self.part = "午後"
-        else:
-            self.part = "夜"
 
     def greet(self, store):
-        self._day()
-        self._part_of_day()
+        day = _day()
+        part = _part_of_day()
         print(f"{store}へようこそ！私、{self.name}と申します。")
-        print(f"{self.day}の{self.part}、いかがお過ごしですか？")
+        print(f"{day}の{part}、いかがお過ごしですか？")
         print(f"本日のお客様に20% OFFのクーポンを差し上げます！")
 
 
