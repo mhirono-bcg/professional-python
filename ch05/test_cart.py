40baaaa46bb00d680610078769aa98ea87b6bee8
Hirono.Masatake@bcg.com <Hirono.Masatake@bcg.com>
2022-05-02 13:16:01 +0900

    defined integrated test

---
+++ ch05/test_cart.py
@@ -0,0 +1,15 @@
+import unittest
+
+from cart import ShoppingCart
+from product import Product
+
+
+class ShoppingCartTestCase(unittest.TestCase):
+    def test_add_and_remove_product(self):
+        cart = ShoppingCart()
+        product = Product("shoes", "S", "blue")
+
+        cart.add_product(product)
+        cart.remove_product(product)
+
+        self.assertDictEqual({}, cart.products)
