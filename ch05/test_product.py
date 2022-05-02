0168cc5fbb17497b62fdafc3b7eb8acefb61b207
Hirono.Masatake@bcg.com <Hirono.Masatake@bcg.com>
2022-05-02 18:40:25 +0900

    finalized the pytest

--- ch05/test_product.py
+++ ch05/test_product.py
@@ -1,23 +1,21 @@
-import unittest
-
 from product import Product
 
 
-class ProductTestCase(unittest.TestCase):
+class TestProduct:
     def test_transform_name_for_sku(self):
         small_black_shoes = Product("shoes", "S", "black")
         expected_value = "SHOES"
         actual_value = small_black_shoes.transform_name_for_sku()
-        self.assertEqual(expected_value, actual_value)
+        assert expected_value == actual_value
 
     def test_transform_color_for_sku(self):
         small_black_shoes = Product("shoes", "S", "black")
         expected_value = "BLACK"
         actual_value = small_black_shoes.transform_color_for_sku()
-        self.assertEqual(expected_value, actual_value)
+        assert expected_value == actual_value
 
     def test_generate_sku(self):
         small_black_shoes = Product("shoes", "S", "black")
         expected_value = "SHOES-S-BLACK"
         actual_value = small_black_shoes.generate_sku()
-        self.assertEqual(expected_value, actual_value)
+        assert expected_value == actual_value
