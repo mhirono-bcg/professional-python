98d3253fbdc5a69d60d747f8ba8027c6c7e932d2
Hirono.Masatake@bcg.com <Hirono.Masatake@bcg.com>
2022-05-02 18:30:00 +0900

    full test suite

--- ch05/test_cart.py
+++ ch05/test_cart.py
@@ -5,11 +5,29 @@ from product import Product
 
 
 class ShoppingCartTestCase(unittest.TestCase):
+    def test_cart_empty(self):
+        cart = ShoppingCart()
+        self.assertDictEqual({}, cart.products)
+
+    def test_add_product(self):
+        cart = ShoppingCart()
+        product = Product("shoes", "S", "blue")
+        cart.add_product(product)
+        expected_value = {"SHOES-S-BLUE": {"quantity": 1}}
+        self.assertDictEqual(expected_value, cart.products)
+
     def test_add_and_remove_product(self):
         cart = ShoppingCart()
         product = Product("shoes", "S", "blue")
-
         cart.add_product(product)
         cart.remove_product(product)
+
+        self.assertDictEqual({}, cart.products)
+
+    def test_add_and_remove_product_excessively(self):
+        cart = ShoppingCart()
+        product = Product("shoes", "S", "blue")
+        cart.add_product(product, quantity=1)
+        cart.remove_product(product, quantity=2)
 
         self.assertDictEqual({}, cart.products)
