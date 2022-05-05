49c2f3e5172341cf884ede895e9135573031cd71
Hirono.Masatake@bcg.com <Hirono.Masatake@bcg.com>
2022-05-05 21:12:14 +0900

    add

---
+++ ch09/book.py
@@ -0,0 +1,50 @@
+# @@range_begin(list1)
+class Book:
+    def __init__(self, data):
+        self.title = data["title"]
+        self.subtitle = data["subtitle"]
+        self.author_data = data["author"]  # <1>
+        self.author = Author(self.author_data)
+
+    @property
+    def author_for_display(self):  # <2>
+        return self.author.for_display
+
+    @property
+    def author_for_citation(self):  # <3>
+        return self.author.for_citation
+
+
+class Author:
+    def __init__(self, author_data):
+        self.first_name = author_data["first_name"]
+        self.last_name = author_data["last_name"]
+
+    @property
+    def for_display(self):  # <2>
+        return f'{self.author_data["first_name"]} {self.author_data["last_name"]}'
+
+    @property
+    def for_citation(self):  # <3>
+        return f'{self.author_data["last_name"]}, {self.author_data["first_name"][0]}.'
+
+
+# @@range_end(list1)
+
+
+# @@range_begin(list2)
+book = Book(
+    {
+        "title": "Brillo-iant",
+        "subtitle": "The pad that changed everything",
+        "author": {
+            "first_name": "Rusty",
+            "last_name": "Potts",
+        },
+    }
+)
+
+
+print(book.author_for_display)  # ウェブ表示用
+print(book.author_for_citation)  # 論文参考文献用
+# @@range_end(list2)
